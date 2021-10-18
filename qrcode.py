dda501c925c756cc3da6de4e0128bb0700bfd9f3
braucci <raucci@gmail.com>
2021-10-18 09:13:06 +0200

    Add files via upload

---
+++ qrcode.py
@@ -0,0 +1,44 @@
+#!/usr/bin/python
+from PyQt6.QtWidgets import QApplication, QWidget
+from PyQt6.QtGui import QPixmap
+from PyQt6 import uic
+import pyqrcode
+from pyqrcode import QRCode
+import sys
+
+class Ui(QWidget):
+    def __init__(self):
+        super().__init__()
+        uic.loadUi('qrcode.ui', self)
+        self.setFixedSize(500, 390)
+    
+
+    def pB_valutaClick(self):
+
+        try:
+            # String which represents the QR code
+            s = (self.lE_testo.text())
+            # output file name
+            filename = "qrcode.png"
+            filename_svg = "qrcode.svg"
+            # Generate QR Code
+            img = pyqrcode.create(s)
+            img.png(filename, scale=7)
+            if self.cB_svg.isChecked():
+                img.svg(filename_svg, scale=8)
+            
+            pixmap = QPixmap(filename)
+            self.lab_output.setPixmap(pixmap)
+
+        except:
+            print('Valore nel campo non accettato')
+            #self.lE_imponibile.setText('')
+            #self.lE_iva.setText('')
+            #self.lE_aliquota.setText('22')
+
+
+
+app = QApplication(sys.argv)
+window = Ui()
+window.show()
+sys.exit(app.exec())
